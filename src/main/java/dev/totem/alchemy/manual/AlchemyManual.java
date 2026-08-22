48f9bc6ffd694bd2f90efcdcf4ac3abd3a1b8b90
阿昀 <yunthomas006@gmail.com>
2026-08-22 21:18:51 +0800

    refactor(manual): separate guides from material research

--- src/main/java/dev/totem/alchemy/manual/AlchemyManual.java
+++ src/main/java/dev/totem/alchemy/manual/AlchemyManual.java
@@ -24,8 +24,9 @@ public final class AlchemyManual {
     private static final AtomicBoolean REGISTERED = new AtomicBoolean();
     private static final Identifier MANUAL_ADVANCEMENT = Identifier.fromNamespaceAndPath("deadrecall", "alchemy_manual");
     private static final List<String> PAGE_KEYS = List.of(
-            "book.deadrecall.alchemy_manual.page.1",
-            "book.deadrecall.alchemy_manual.page.2",
+            "book.totem_alchemy.guide.principles",
+            "book.totem_alchemy.guide.brewing_stand",
+            "book.totem_alchemy.guide.cauldron",
             "book.totem_alchemy.material_slot.nether_wart",
             "book.totem_alchemy.material_slot.red_mushroom",
             "book.totem_alchemy.material_slot.spider_eye",
